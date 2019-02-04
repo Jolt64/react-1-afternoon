4ff5c6d8bdb0402b2075eea85edee6488da0607a
Jolt64 <jonathantelford64@gmail.com>
2019-02-03 22:27:31 -0700

    First Two Are Good

---
+++ src/components/Topics/Palindrome.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class Palindrome extends Component{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            <p>Palindrome</p>
+        )
+    }
+}
+
+
+export default Palindrome;
